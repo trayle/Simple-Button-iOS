47ca12841b523d217e329febbdae535151858ea8
Tim <tmr@knoable.com>
2016-06-24 11:39:47 -0700

    Initial Commit

---
+++ Simple Button/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Button
+//
+//  Created by Tim on 6/24/16.
+//  Copyright © 2016 Tim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
